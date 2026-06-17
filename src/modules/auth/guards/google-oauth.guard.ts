5154f0aa973f21216a115baea45bbfcc0f1c56b1
Shivam Maurya <shivammaurya011@gmail.com>
2026-06-17 17:18:36 +0530

    Fix: Oauth bug fix

--- src/modules/auth/guards/google-oauth.guard.ts
+++ src/modules/auth/guards/google-oauth.guard.ts
@@ -1,10 +1,11 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Injectable()
 export class GoogleOAuthGuard extends AuthGuard('google') {
   getAuthenticateOptions(context: ExecutionContext) {
-    const req = context.switchToHttp().getRequest();
+    const req = context.switchToHttp().getRequest<Request>();
     return {
       state: req.query.state as string,
     };
